c5d2c6c2188e8c942ee9c23b7b6ac2d908f66ee5
Benjamin Porter <bporter816@gmail.com>
2022-08-05 21:27:34 -0500

    Add git branch switcher and rename general.zsh to force it to load first

--- zsh/dot-zsh/git.zsh
+++ zsh/dot-zsh/git.zsh
@@ -1,2 +1,7 @@
 alias g="git"
 alias gt='cd $(git rev-parse --show-toplevel)'
+
+# Switch to a git branch by grepping for branch name
+function br() {
+    git branch | rg $1 | head -n 1 | lc | xargs git checkout
+}
