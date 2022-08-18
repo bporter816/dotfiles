d78dc23c63ebde52b44d0f5c0f09daa487174b98
Benjamin Porter <bporter816@gmail.com>
2022-08-17 20:51:57 -0500

    Add a bunch of shell git aliases

--- zsh/dot-zsh/git.zsh
+++ zsh/dot-zsh/git.zsh
@@ -2,8 +2,3 @@ alias g="git"
 
 # Go to the root of the current repo
 alias gt='cd $(git rev-parse --show-toplevel)'
-
-# Switch to a git branch by grepping for the branch name
-function br() {
-    git branch | rg $1 | head -n 1 | lc | xargs git checkout
-}
