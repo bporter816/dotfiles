b5a4a6ce19c56ce23adc6c2f439217a6493154e8
Benjamin Porter <bporter816@gmail.com>
2022-10-24 02:42:18 -0500

    Initial nvim config

---
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -0,0 +1,28 @@
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+-- leader
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+keymap("i", "kj", "<Esc>", opts)
+
+-- better split navigation
+keymap("n", "<C-h>", "<C-w>h>", opts)
+keymap("n", "<C-j>", "<C-w>j>", opts)
+keymap("n", "<C-k>", "<C-w>k>", opts)
+keymap("n", "<C-l>", "<C-w>l>", opts)
+
+-- buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- resizing
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+keymap("n", "<leader>e", ":Lex 30<CR>", opts)  -- toggle file explorer
+keymap("n", "<leader>r", ":set relativenumber!<CR>", opts)
---
+++ nvim/.config/nvim/lua/user/options.lua
@@ -0,0 +1,30 @@
+local options = {
+    number = true,
+    relativenumber = false,
+    cursorline = true,
+    colorcolumn = "81,121",
+
+    splitbelow = true,
+    splitright = true,
+
+    mouse = "a",
+
+    wrap = false,
+
+
+    -- searching
+    ignorecase = true,
+    hlsearch = true,
+
+    -- indentation
+    expandtab = true,
+    smarttab = true,
+    tabstop = 4,
+    shiftwidth = 4,
+}
+
+vim.opt.iskeyword:append "-" -- include dashes in words
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
