c5bd232669ce2c22aeb635fcd3fe257b4b41fb3e
Benjamin Porter <bporter816@gmail.com>
2022-10-25 02:56:58 -0500

    Add nvim cmp

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -14,6 +14,20 @@ if not status_ok then
 end
 
 return packer.startup(function(use)
+    -- packer
     use { "wbthomason/packer.nvim" }
+
+    -- colorschemes
     use { "ellisonleao/gruvbox.nvim" }
+
+    -- cmp
+    use { "hrsh7th/nvim-cmp" }
+    use { "hrsh7th/cmp-buffer" }
+    use { "hrsh7th/cmp-path" }
+    use { "hrsh7th/cmp-cmdline" }
+    use { "saadparwaiz1/cmp_luasnip" }
+
+    -- snippets
+    use { "L3MON4D3/LuaSnip" }
+    use { "rafamadriz/friendly-snippets" }
 end)
