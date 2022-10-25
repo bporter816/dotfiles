64dbb7b9d9f2c5687942ee44b70a0e6858731633
Benjamin Porter <bporter816@gmail.com>
2022-10-25 00:30:24 -0500

    Add packer for nvim plugins

---
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -0,0 +1,18 @@
+vim.cmd [[packadd packer.nvim]]
+
+vim.cmd([[
+    augroup packer_user_config
+        autocmd!
+        autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    augroup end
+]])
+
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+    vim.notify("packer not found!")
+    return
+end
+
+return packer.startup(function(use)
+    use { "wbthomason/packer.nvim" }
+end)
