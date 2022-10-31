694eb751d3b77819df9b79bf2b86d467ff34999d
Benjamin Porter <bporter816@gmail.com>
2022-10-30 22:47:24 -0500

    Update nvim options

--- nvim/.config/nvim/lua/user/options.lua
+++ nvim/.config/nvim/lua/user/options.lua
@@ -1,29 +1,33 @@
 local options = {
+    -- visual
     number = true,
     relativenumber = false,
     cursorline = true,
     colorcolumn = "81,121",
+    laststatus = 2, -- always show status line
+    wrap = false,
 
     termguicolors = true,
     background = "dark",
 
+    -- utilities
+    mouse = "a", -- enable mouse in all modes
+    scrolloff = 10,
     splitbelow = true,
     splitright = true,
-
-    mouse = "a",
-
-    wrap = false,
 
 
     -- searching
     ignorecase = true,
-    hlsearch = true,
+    hlsearch = true, -- highlight search results
+    incsearch = true, -- highlight matches while typing the search
 
     -- indentation
     expandtab = true,
     smarttab = true,
-    tabstop = 4,
+    softtabstop = 4,
     shiftwidth = 4,
+    tabstop = 4,
 }
 
 vim.opt.iskeyword:append "-" -- include dashes in words
