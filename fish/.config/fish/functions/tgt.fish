662a1b17242ecf0683bc724e9a09e07dd4e41fe0
Benjamin Porter <bporter816@gmail.com>
2023-01-17 11:37:28 -0500

    Fix xargs

--- fish/.config/fish/functions/tgt.fish
+++ fish/.config/fish/functions/tgt.fish
@@ -1,3 +1,3 @@
 function tgt --description "grep for terragrunt targets to perform an action on"
-    terragrunt state list | rg $argv[2] | awk '$0="-target="$0' | xargs -d '\n' -o -r terragrunt $argv[1]
+    terragrunt state list | rg $argv[2] | awk '$0="-target="$0' | xargs -o -r terragrunt $argv[1]
 end
