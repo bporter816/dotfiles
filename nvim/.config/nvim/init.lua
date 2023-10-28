3b621fb693d1c53a2e01a1c48dfaada318fcc503
Benjamin Porter <bporter816@gmail.com>
2023-10-28 02:07:26 -0400

    Update nvim plugin manager

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,5 @@
 require "user.options"
-require "user.keymaps"
+require "user.keymaps" -- set keymaps before loading plugins so leader is correct
 require "user.plugins"
 require "user.colorscheme"
 require "user.cmp"
--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -1,48 +1,50 @@
-vim.cmd [[packadd packer.nvim]]
-
-vim.cmd([[
-    augroup packer_user_config
-        autocmd!
-        autocmd BufWritePost plugins.lua source <afile> | PackerSync
-    augroup end
-]])
-
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-    vim.notify("packer not found!")
-    return
-end
-
-return packer.startup(function(use)
-    -- packer
-    use { "wbthomason/packer.nvim" }
-
+plugins = {
     -- colorschemes
-    use { "morhetz/gruvbox" }
-    use { "folke/tokyonight.nvim" }
-    use { "NLKNguyen/papercolor-theme" }
+    "morhetz/gruvbox",
+    "folke/tokyonight.nvim",
+    "NLKNguyen/papercolor-theme",
 
     -- languages
-    use { "hashivim/vim-terraform" }
-    use { "udalov/kotlin-vim" }
-    use { "dag/vim-fish" }
-
-    -- cmp
-    use { "hrsh7th/nvim-cmp" }
-    use { "hrsh7th/cmp-buffer" }
-    use { "hrsh7th/cmp-path" }
-    use { "hrsh7th/cmp-cmdline" }
-    use { "saadparwaiz1/cmp_luasnip" }
-
-    -- snippets
-    use { "L3MON4D3/LuaSnip" }
-    use { "rafamadriz/friendly-snippets" }
-
-    -- utils
-    use { "junegunn/fzf" }
-    use { "junegunn/fzf.vim" }
+    "hashivim/vim-terraform",
+    "udalov/kotlin-vim",
+    "dag/vim-fish",
 
     -- git
-    use { "tpope/vim-fugitive" }
-    use { "tpope/vim-rhubarb" }
-end)
+    "tpope/vim-fugitive",
+    "tpope/vim-rhubarb",
+}
+
+opts = {}
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup(plugins, opts)
+
+
+--    -- cmp
+--    use { "hrsh7th/nvim-cmp" }
+--    use { "hrsh7th/cmp-buffer" }
+--    use { "hrsh7th/cmp-path" }
+--    use { "hrsh7th/cmp-cmdline" }
+--    use { "saadparwaiz1/cmp_luasnip" }
+--
+--    -- snippets
+--    use { "L3MON4D3/LuaSnip" }
+--    use { "rafamadriz/friendly-snippets" }
+--
+--    -- utils
+--    use { "junegunn/fzf" }
+--    use { "junegunn/fzf.vim" }
+--
+--end)
