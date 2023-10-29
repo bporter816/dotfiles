fe1b6ca529d4d670fca788d1b29e6fbe3dc80c4b
Benjamin Porter <bporter816@gmail.com>
2023-10-29 18:00:11 -0400

    Add nvim completion back

--- nvim/.config/nvim/lua/user/lazy.lua
+++ nvim/.config/nvim/lua/user/lazy.lua
@@ -15,9 +15,6 @@ require("lazy").setup({ import = "user.plugins" })
 
 
 --    -- cmp
---    use { "hrsh7th/nvim-cmp" }
---    use { "hrsh7th/cmp-buffer" }
---    use { "hrsh7th/cmp-path" }
 --    use { "hrsh7th/cmp-cmdline" }
 --    use { "saadparwaiz1/cmp_luasnip" }
 --
