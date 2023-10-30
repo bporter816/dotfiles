19bb4b88e977f43e7f82cb7d4d6201fa2b8b80ed
Benjamin Porter <bporter816@gmail.com>
2023-10-29 23:19:08 -0400

    Add LSP in autocomplete

--- nvim/.config/nvim/lua/user/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/user/plugins/lspconfig.lua
@@ -1,10 +1,15 @@
 return {
     {
         "neovim/nvim-lspconfig",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+        },
         config = function()
             local lspconfig = require("lspconfig")
-            lspconfig["gopls"].setup({})
-            lspconfig["terraformls"].setup({})
+            local cmp_nvim_lsp = require("cmp_nvim_lsp")
+            local capabilities = cmp_nvim_lsp.default_capabilities()
+            lspconfig["gopls"].setup({ capabilities = capabilities })
+            lspconfig["terraformls"].setup({ capabilities = capabilities })
         end,
     },
 }
--- nvim/.config/nvim/lua/user/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/user/plugins/nvim-cmp.lua
@@ -5,12 +5,21 @@ return {
         dependencies = {
             "hrsh7th/cmp-buffer",
             "hrsh7th/cmp-path",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
         },
         config = function()
             local cmp = require("cmp")
+            local luasnip = require("luasnip")
             cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
                 sources = cmp.config.sources({
-                    --{ name = "nvim_lsp" },
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
                     { name = "buffer" },
                     { name = "path" },
                 }),
