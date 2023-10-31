a557e87d3d553437c1ae958bf194cf469e1fb2b9
Benjamin Porter <bporter816@gmail.com>
2023-10-31 17:15:44 -0400

    Add LSP shortcuts

--- nvim/.config/nvim/lua/user/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/user/plugins/lspconfig.lua
@@ -23,6 +23,17 @@ return {
             lspconfig["yamlls"].setup({ capabilities = capabilities })
             lspconfig["pyright"].setup({ capabilities = capabilities })
             lspconfig["bashls"].setup({ capabilities = capabilities })
+
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+                callback = function(ev)
+                    local opts = { buffer = ev.buf }
+                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+                end,
+            })
         end,
     },
 }
