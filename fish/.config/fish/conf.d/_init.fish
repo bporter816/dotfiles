a27b093eb7d2ea2bbea117c4be7ad59c21f59cc3
Benjamin Porter <bporter816@gmail.com>
2025-07-02 01:56:13 -0700

    Manage node and python with asdf

--- fish/.config/fish/conf.d/_init.fish
+++ fish/.config/fish/conf.d/_init.fish
@@ -7,18 +7,19 @@ if status is-interactive
     fish_vi_key_bindings
     bind -M insert -m default kj 'commandline -f repaint'
 
-    # pyenv setup
-    if command -v pyenv > /dev/null 2>&1
-        set -Ux PYENV_ROOT $HOME/.pyenv
-        fish_add_path $PYENV_ROOT/bin
-        pyenv init - | source
+    # ASDF configuration code
+    if test -z $ASDF_DATA_DIR
+        set _asdf_shims "$HOME/.asdf/shims"
+    else
+        set _asdf_shims "$ASDF_DATA_DIR/shims"
     end
 
-    # nvm setup
-    set -gx NVM_DIR ~/.nvm
-    if test -s "$NVM_DIR/nvm.sh"
-        bass source "$NVM_DIR/nvm.sh"
+    # Do not use fish_add_path (added in Fish 3.2) because it
+    # potentially changes the order of items in PATH
+    if not contains $_asdf_shims $PATH
+        set -gx --prepend PATH $_asdf_shims
     end
+    set --erase _asdf_shims
 
     # zoxide setup
     if command -v zoxide > /dev/null 2>&1
