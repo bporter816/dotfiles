5b2f082acacaa25c802d2afdda0208339f8fcf2e
Benjamin Porter <bporter816@gmail.com>
2025-07-02 22:53:28 -0400

    Remove last pieces of fisher, add asdf plugins

--- setup.sh
+++ setup.sh
@@ -2,39 +2,33 @@
 
 mkdir -p ~/personal/
 
-if [ -d ~/personal/fisher/ ]; then
-    echo "fisher is already installed"
+if command -v brew &>/dev/null; then
+	echo "brew is already installed"
 else
-    echo "Installing fisher..."
-    git clone https://github.com/jorgebucaran/fisher ~/personal/fisher
+	echo "brew not installed, installing"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-mkdir -p ~/.config/fisher/{completions,functions}/
-ln -s ~/personal/fisher/completions/fisher.fish ~/.config/fisher/completions/fisher.fish
-ln -s ~/personal/fisher/functions/fisher.fish ~/.config/fisher/functions/fisher.fish
-
-if command -v brew &> /dev/null; then
-    echo "brew is already installed"
-else
-    echo "brew not installed, installing..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-echo "Installing brew formulae..."
+echo "Installing brew formulae"
 formulae=$(brew ls)
 for i in $(cat brew_formulae); do
-    if rg -q $i <<< $formulae; then
+	if rg -q $i <<<$formulae; then
 		echo "    $i already installed"
-    else
-        echo "    Installing $i"
-        brew install $i
-    fi
+	else
+		echo "    Installing $i"
+		brew install $i
+	fi
 done
 
-echo "Linking dotfiles..."
+echo "Linking dotfiles"
 for i in *; do
-    if [[ -d $i ]]; then
-        echo "    Linking $i"
-        stow -t $HOME $i
-    fi
+	if [[ -d $i ]]; then
+		echo "    Linking $i"
+		stow -t $HOME $i
+	fi
 done
+
+echo "Installing asdf plugins"
+asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin add python https://github.com/asdf-community/asdf-python.git
